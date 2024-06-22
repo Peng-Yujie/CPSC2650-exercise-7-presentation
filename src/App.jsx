02f879ee760fcf2cc93c2c9d3c5a1fafd9109728
Yujie Peng <ypeng24@mylangara.ca>
2024-06-22 09:48:24 -0700

    create project with vite

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>CPSC2650 Week7 Presentation</h1>
+      <p>
+        This is a simple React + Vite app, built for the CPSC2650 Week7
+        presentation.
+      </p>
+    </>
+  );
+}
+
+export default App;
