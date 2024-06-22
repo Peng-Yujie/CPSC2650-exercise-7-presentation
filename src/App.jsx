2f6eac1f370d06d9fca54b9c5a6e472c638e383f
Yujie Peng <ypeng24@mylangara.ca>
2024-06-22 09:57:36 -0700

    added textarea

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import Textarea from "./components/Textarea";
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
         This is a simple React + Vite app, built for the CPSC2650 Week7
         presentation.
       </p>
+      <Textarea name="Yujie" />
     </>
   );
 }
---
+++ src/components/Textarea.jsx
@@ -0,0 +1,5 @@
+function Textarea({ name }) {
+  return <p>{name}</p>;
+}
+
+export default Textarea;
